5930eeaad26e9958d6c19212a3942da2ea8b11e1
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-10 14:16:39 +0700

    update: added summary about any type

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -23,3 +23,11 @@ let point: { x: number; y: number } = { x: 12, y: 13 };
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// Any Type Example
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+/* 
+note: Kalo di perhatikan constanta coordinates disini menggunakan any type.
+kenapa? karena function JSON.parse() akan men-parse nilai dari property yg akan di parse sesuai dengan tipe data nilainya. Maka dari itu typescript tidak dapat memprediksi type apa yang digunakan oleh si constanta coordinates
+*/
