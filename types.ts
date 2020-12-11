1cbe919093e4bd76042dace72e79750683a098cf
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-11 18:49:24 +0700

    update: fixing information inference in types file

--- types.ts
+++ types.ts
@@ -13,7 +13,9 @@ const red = new Color();
 Type Annotation, seperti namanya annotations dalam bahasa indonesia artinya Penjelasan. 
 jadi dalam typescript ketika kita memberikan sebuah type dalam variable, function maupun parameter itu kita sebut sebagai type annotations.
 
-Kemudian untuk type inference, dia seperti proses di belakang layar dari type annotation. Dimana ketika kita sudah memberikan type pada sebuah varaible kemudian menghapus typenya. Variable tsb masih akan membaca type yang sebelumnya sudah kita berikan. Dengan ketentuan pemberian type pada variable tsb dalam satu baris.
+Kemudian untuk type inference, dia seperti proses di belakang layar dari type annotation. Dimana ketika kita sudah memberikan type pada sebuah variable ataupun function kemudian menghapus typenya. Variable tsb masih akan membaca type yang sebelumnya sudah kita berikan. 
+Dengan Catatan :
+- jika memberikan type pada sebuah ariable, maka variable dengan nilainya harus dalam satu baris. Jika tidak typescript akan memberikan any type kepada variable tsb.
 
 
 Kapan kita harus menggunakan Type Annotations?
