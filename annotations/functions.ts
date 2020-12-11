b8eb31a2e9561322469a421a6387ab90a96a515b
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-11 20:47:13 +0700

    update: added example type annotations in anonymous function

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -6,3 +6,16 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number) => {
   return a - b;
 };
+
+function devide(a: number, b: number): number {
+  return a / b;
+}
+
+// Anonymus Function Example
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const add2 = (a: number, b: number): number => {
+  return a + b;
+};
