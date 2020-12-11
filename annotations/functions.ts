868ea0ad1eaff7f134f1516404300243a160cc5b
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-11 20:58:31 +0700

    update: added example never type

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -19,3 +19,15 @@ const multiply = function (a: number, b: number): number {
 const add2 = (a: number, b: number): number => {
   return a + b;
 };
+
+// Void Type Example, note : type void ini digunakan ketika function yang kita buat tidak mengembalikan nilai apa-apa. Secara teknikal void juga bisa me-return null atau undefined
+
+const logger = (message: string): void => {
+  console.log(message);
+  // return null; it's okey
+};
+
+// Example Never Type
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
