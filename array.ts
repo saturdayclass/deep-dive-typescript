897ebdc9614258b33096cf9377c6ccd3706b40e5
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-14 17:07:16 +0700

    update: added information why do we care about array in ts

--- array.ts
+++ array.ts
@@ -2,3 +2,10 @@
 const carMakers: string[] = ['Toyota', 'Ford', 'BMW'];
 const dates = [new Date(), new Date(), new Date()];
 const carsByMakes: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+/* Why do we care?
+1). Ts can co type inference when extracting values from an array
+2). Ts can prevent us from adding incompatible values to the array
+3). We can get help with 'map', 'forEach, 'reduce' function
+4). Flexible - arrays can be still contain multiple different types
+*/
