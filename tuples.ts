12f31a446d3525d97d661ac06aee490757ccbc58
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-16 19:42:01 +0700

    update: fixing name file of tuple.ts to tuples.ts

---
+++ tuples.ts
@@ -0,0 +1,4 @@
+// Tuple
+// Struktur data yang sangat mirip sekali dengan array. Tapi bukan array,
+// Tupple biasa nya mengandung beberapa property untuk mendiskripsikan satu hal
+// Biasa nya tupple akan mencocokan banyak type yang berbeda dalam data yang ada dalam tupple
