dcfbd302511f6de0ca75ea888fdc51f20c8fadb2
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-19 13:48:16 +0700

    update: added example problem if without using interface in typescript

--- interface.ts
+++ interface.ts
@@ -1,1 +1,17 @@
 // Interface adalah sebuah feature dalam typescript yang digunakan untuk membuat sebuah type baru yang mendeskripsikan nama property dan value pada sebuah object.
+
+const oldCivic = {
+  name: 'civic',
+  year: 2002,
+  broken: true,
+};
+
+const printVechile = (vechile: {
+  name: string;
+  year: number;
+  broken: boolean;
+}): void => {
+  console.log(`Name : ${vechile.name}`);
+  console.log(`Year : ${vechile.year}`);
+  console.log(`Broken? ${vechile.broken}`);
+};
