eafc4d63437a6bdcd116987187caef41616c5c89
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-19 14:08:02 +0700

    update: added example interface in typescript

--- interface.ts
+++ interface.ts
@@ -1,17 +1,29 @@
 // Interface adalah sebuah feature dalam typescript yang digunakan untuk membuat sebuah type baru yang mendeskripsikan nama property dan value pada sebuah object.
 
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2002,
   broken: true,
 };
 
-const printVechile = (vechile: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
-  console.log(`Name : ${vechile.name}`);
-  console.log(`Year : ${vechile.year}`);
-  console.log(`Broken? ${vechile.broken}`);
+// const printVehicle = (vehicle: {
+//   name: string;
+//   year: number;
+//   broken: boolean;
+// }): void => {
+//   console.log(`Name : ${vehicle.name}`);
+//   console.log(`Year : ${vehicle.year}`);
+//   console.log(`Broken? ${vehicle.broken}`);
+// };
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name : ${vehicle.name}`);
+  console.log(`Year : ${vehicle.year}`);
+  console.log(`Broken? ${vehicle.broken}`);
 };
